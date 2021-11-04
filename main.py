aaba1fb35b06874fc86dcbddc6942a0b34b8ca7d
Nacho <ignacioalfaro99@gmail.com>
2021-11-04 17:55:17 -0600

    Agregando el respositorio y el main

---
+++ main.py
@@ -0,0 +1,10 @@
+from azure.cognitiveservices.vision.customvision import prediction
+from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
+from msrest.authentication import ApiKeyCredentials
+import os, time, uuid, requests
+
+
+ENDPOINT = "" #Agregar el endpoint
+prediction_key = "" #Agregar el key para el endpoint 
+
+
